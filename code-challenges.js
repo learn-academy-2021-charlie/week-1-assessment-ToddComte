e27b49d7e76364f9263803629b0e875ad03f49b2
ToddComte <comtetodd@gmail.com>
2021-07-12 09:35:44 -0600

    completed

--- code-challenges.js
+++ code-challenges.js
@@ -10,45 +10,154 @@
 // --------------------1) Create a function that takes in a number and determines if a temperature is below boiling point, at boiling point or above boiling point. Boiling point is 212 degrees Fahrenheit.
 // Use the test variables provided below. Expected output: "35 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp1 = 35
-var temp2 = 350
-var temp3 = 212
+// var temp1 = 35
+// var temp2 = 350
+// var temp3 = 212
 
+// const boilingPoint = (temp) => {
+//     if (temp < 212){
+//         return `${temp} is below boiling point`
+//     } else if (temp > 212){
+//         return `${temp} is above boiling point`
+//     } else if (temp === 212){
+//         return `${temp} is at boiling point`
+//     } else {
+//         return "ERROR"
+//     }
+// }
 
+// console.log(boilingPoint(212))
+//
+/////////////////////////////////////////////////////////////////////// Todd's Pseudocode //////////////////////////////////////////////////////////////
+// When it came to coding the problem above I knew that I needed to create an if statement and at least two other else if statements.
+// Once I checked to see if the first if statement worked which it did.
+// I then just repeated the same steps for my else if statements.
+// Then last and not least I put the old statement of returning the error just in case the user did not put in a number.
+// I know it probably took me a little longer than most people because I would write a little bit of code and test it out.
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // --------------------2) Create a function that takes in two arrays of numbers and returns a single array with all the values sorted least to greatest.
 // Use the test variables provided below. Expected output: [-9, -7, 0, 3, 7, 8, 9, 13, 22, 36]
 
-var myNumbers1 = [3, 7, 0, 36, -9]
-var myNumbers2 = [8, -7, 22, 9, 13]
+// var myNumbers1 = [3, 7, 0, 36, -9]
+// var myNumbers2 = [8, -7, 22, 9, 13]
+// var myNewNumber1 = myNumbers1.slice(4)
+// var myNewNumber2 = myNumbers1.slice(3,4)
+// var myNewNumber3 = myNumbers1.slice(2,3)
+// var myNewNumber4 = myNumbers1.slice(1,2)
+// var myNewNumber5 = myNumbers1.slice(0,1)
+// var myNewNumber6 = myNumbers2.slice(4)
+// var myNewNumber7 = myNumbers2.slice(3,4)
+// var myNewNumber8 = myNumbers2.slice(2,3)
+// var myNewNumber9 = myNumbers2.slice(1,2)
+// var myNewNumber10 = myNumbers2.slice(0,1)
+// var myArray = myNewNumber1.concat(myNewNumber9).concat(myNewNumber3).concat(myNewNumber5).concat(myNewNumber4).concat(myNewNumber10).concat(myNewNumber7).concat(myNewNumber6).concat(myNewNumber8).concat(myNewNumber2)
+// console.log(myArray)
+/////////////////////////////////////////////////////////////////////// Todd's Pseudocode //////////////////////////////////////////////////////////////
+// All I have to say is you do the best You can do with what you have.
+//I took what I had learned during the week and stuck with what I know I understand that it's probably a long drawn out process but I got the job done.
+//I knew that by using the slice method I would be able to chop a single number out of an array so that's what I did I chopped it 10 times for 10 numbers.
+//Then I figured out which number went where and used the concat method ten times to make a new variable called myArray.
+// After I had done all of this I wanted to know if there was a simpler way so I got online and talked to a couple of other coders that I know and this was the solution they gave me.
+// 
+// var myNewNumber = myNumbers1.concat(myNumbers2)
+// // console.log(myNewNumber)
 
-
+// var myNewArray = myNewNumber.sort(function(a,b){ return a - b});
+// console.log(myNewArray)
+//This is a function that allows you to take an array break it down by numbers. 
+// sort that array then return the assorted numbers.
+//Reading the syllabus we should be learning about these types of functions next week
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // --------------------3) Create a function that takes in a string of a single word and returns the string with all letters reversed.
 // Use the test variables provided below. Expected output: "ovarb", "eilrahc"
 
-var myString1 = "bravo"
-var myString2 = "charlie"
+// var myString1 = "bravo"
+// var myString2 = "charlie"
 
-
+// var newString3 = myString1.split("")
+// var newString4 = newString3.reverse()
+// var newString5 = myString2.split("")
+// var newString6 = newString5.reverse()
+// var newString7 = newString4.join("")
+// var newString8 = newString6.join("")
+// console.log(newString7, newString8)
+/////////////////////////////////////////////////////////////////////// Todd's Pseudocode //////////////////////////////////////////////////////////////
+// First thing I had to do was convert the strings into an array because the .reverse is an a ray method.
+// As you can see I worked with one and then the other just to make things simple for me.
+// Once I got them into a race I then reversed them and then using the .join Method I was then able to convert them back into a string.
+// Then I worked on the second bit of code for Charlie once I knew that I had bravo correct.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // --------------------4) Create a function that takes in two numbers and subtracts the smaller number from the larger number.
 // Use the two sets of test variables provided below. Expected output: 15, 12
 
-var numberExample1 = 42
-var numberExample2 = 27
+// var numberExample1 = 42
+// var numberExample2 = 27
 
-var numberExample3 = 7
-var numberExample4 = 19
+// var numberExample3 = 19
+// var numberExample4 = 17
 
+// const equation1 = (answer) => {
+//     if(numberExample1 > numberExample2) {
+//         return numberExample1 - numberExample2
+//     } else if (numberExample1 < numberExample2) {
+//         return `${numberExample1} is smaller then ${numberExample2} so therefore I cannot compute`
+//     }
+// }
+// console.log(equation1())
+
+// const equation2 = (answer) => {
+//     if(numberExample3 > numberExample4) {
+//         return numberExample3 - numberExample4
+//     } else if (numberExample3 < numberExample4) {
+//         return `${numberExample3} is smaller then ${numberExample4} so therefore I cannot compute`
+//     }
+// }
+// console.log(equation2())
+/////////////////////////////////////////////////////////////////////// Todd's Pseudocode //////////////////////////////////////////////////////////////////
+// This is another question where I knew the answer and then I started typing and getting ahead of myself and not really visualizing how the code was going to look and in what order.
+// So they were a few stumbling blocks that I had to slow down and really think through it.
+// Then I saw that Sara One of the instructors were on and asked for help.
+// The only hint she gave me was to check my parentheses.
+// As I said before I was just in too big of a hurry and totally missed a set of parentheses in my consul log. 
+// Once I had fix that problem the code ran perfectly. 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 // --------------------5) Copy/paste your code from #4. Refactor your code to also accept non-number edge cases. Inform your user if the variable passed into the subtraction function is not a number.
 // Use the two sets of test variables provided below. Expected output: "Your input is not a number", 3
 // Hint: There is an operator in JavaScript called typeof :)
 
-var numberExampleRefactor1 = 42
-var numberExampleRefactor2 = "hello"
+// var numberExampleRefactor1 = 42
+// var numberExampleRefactor2 = "hello"
 
-var numberExampleRefactor3 = 27
-var numberExampleRefactor4 = 24
+// var numberExampleRefactor3 = 27
+// var numberExampleRefactor4 = 24
+
+// const equation1 = (answer) => {
+//     if(numberExampleRefactor1 > numberExampleRefactor2) {
+//         return numberExampleRefactor1 - numberExampleRefactor2
+//     } else if (numberExampleRefactor1 < numberExampleRefactor2) {
+//         return `${numberExampleRefactor1} is smaller then ${numberExampleRefactor2} so therefore I cannot compute`
+//     } else {
+//         return `Your input is not a ${typeof numberExampleRefactor1}`
+//     }
+// }
+// console.log(equation1())
+
+// const equation2 = (answer) => {
+//     if(numberExampleRefactor3 > numberExampleRefactor4) {
+//         return numberExampleRefactor3 - numberExampleRefactor4
+//     } else if (numberExampleRefactor3 < numberExampleRefactor4) {
+//         return `${numberExampleRefactor3} is smaller then ${numberExampleRefactor4} so therefore I cannot compute`
+//     }
+// }
+// console.log(equation2())
+/////////////////////////////////////////////////////////////////////// Todd's Pseudocode //////////////////////////////////////////////////////////////////
+// When I read through this the first time I was lost for about 20 minutes until I read up on typeof.
+// And because it took me that time I was able to slow down visualize how the code was going to look and what areas I needed to fix in order for it to display the right output.
+// Finding out that the command typeof basically gives you the data type of the input that you have put in.
+// If you put in a string, boolean, object or even a funcation it will show you.
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
--- interview-questions.md
+++ interview-questions.md
@@ -7,47 +7,47 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a function? Why would you use one?
 
-  Your answer:
+  Your answer: It has been my experience that functions are like Legos And without them doing their job you would never be able to build a Lego sculpture. What I mean by that is it takes functions to build up your porject. Functions are useful in not only JavaScript but all different programming languages, Because they are used as building blocks in any project you want to accomplish. 
 
-  Researched answer:
+  Researched answer: According to the syllabus "functions are a fundamental building block in JavaScript and in programming in general. Functions are reusable pieces of code that only execute when invoked and always return an output." By reading that it lets me know that I was on the right track with my answer. 
 
 
 
 2. In regards to functions, what is the difference between an argument and a parameter?
 
-  Your answer:
+  Your answer: A parameter is an external source of information why the arguments process that parameter for a outcome For instants if I was designing a function for a Thermometer that told you if the temperature was above or below the boiling point. I would first create a parameter called boiling point then create the Arguments that if the temperature was below 212° it would display and tell you so if it was higher and it will tell you that if you were right on the money it would tell you that you were at the boiling point.
 
-  Researched answer:
+  Researched answer: Doing the research I found that I'm on the right track but the way that my answer explains it the function wouldn't be called boiling point it would be called temp where you could put in the temperatures so that the code would run in order to know if your above, below or right on the money.
 
 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: An index is a way to identify elements and what place they hold.So therefore the difference between a value and an index is that the index is a placeholder starting at zero and counting up and a value is the actual value of what you've put in, like data type number, string And so forth.
 
-  Researched answer:
+  Researched answer: According to my research of value is part of an array. The array can hold as many values Do you want and there are certain commands that you can use to change the values in an array. Now an index is a way to find out how many values there are in that array and what number order they're in there is such a thing called zero indexing.
 
 
 
 4. What is a built-in method?
 
-  Your answer:
+  Your answer: These are methods that are built into the program to help us code quicker and easier. They are tools for developers to use such as a mechanic uses a wrench we use built-in methods. Could you imagine if we didn't have them we would have to re-create the same methods over and over and over again. It's like asking a mechanic to do anything without using a wrench. It's not that it's impossible it would just take a lot longer to accomplish whatever you were trying to do.
 
-  Researched answer:
+  Researched answer: There are many built in methods in arrays, Because arrays sotre alot of date And the built-in methods make it easy for us to manipulate that data. We used a lot of them in our Assessments this week. A built-in method is a small piece of code that has been added to accomplish a common task. 
 
 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: There is always a driver and a navigator. It is the drivers job to be the one to code and it is the navigators job to help guide and direct the driver. This does not mean that the navigator forces the driver to go his direction all the time. It means that the navigator allows for clear and concise communication between the two of them so that they can accomplish the goals. In class when we did the challenges me and my Partner would designate how many challenges they were going to do versus how many challenges I was going to do. It is very important for you to switch places and get comfortable being in both roles.
 
-  Researched answer:
+  Researched answer: In my research I found out that I nailed it right on the head. Pair programming is a great technique for developers to collaborate on projects. It allows for the saying two heads are better than one to be true. When I first heard the term pair programming I thought it was going to suck. I like working by myself and asking questions when I need help. It's very nerve-racking to me to have somebody always looking over my shoulder. But the more we have done this I have realized that it makes it easier to ask questions because I already have a partner looking over my shoulder.
 
 
 
 6. What is something we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: The assessment I really found it to be not only challenging but it reinforced all that we had learned over the week. When I heard that you guys were giving us assessment and then saw how big it was I thought great there goes my whole weekend but it didn't take up that much time. I broke it up into sections got that section done took a break and then came back to it. It's going to be really interesting when we're all done and I can look back and read this and see the difference between my coding now versus four months from now. 
 
 
 
@@ -55,12 +55,12 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Iteration:
+1. Iteration: This is a term used to describe how many times something is done. 
 
-2. Higher Order Functions:
+2. Higher Order Functions: This is a term used to describe a function that uses other functions in its argument
 
-3. Jest:
+3. Jest: This is a program that runs through your JavaScript to make sure that you have clean code
 
-4. Classes:
+4. Classes: These have to do with objects and giving them the blueprints of how to work and they have their own scope
 
-5. React:
+5. React: React is a JavaScript library used to build user interfaces
